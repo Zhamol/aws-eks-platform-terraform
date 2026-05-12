921e68bd3eff474dea5efe868bba18cf86f8c609
Zhamoliddin <zhamoliddin.zha@edu.312school.com>
2026-05-11 22:18:55 -0400

    Fix: add tflint config, remove stray bootstrap.sh

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "lab" {
