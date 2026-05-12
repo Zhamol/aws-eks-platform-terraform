61b2d518039f0493c4733db724aea621bdcf6ce1
Zhamoliddin <zhamoliddin.zha@edu.312school.com>
2026-05-11 21:45:46 -0400

    Fix: add required_version and required_providers to all modules

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "lab" {
